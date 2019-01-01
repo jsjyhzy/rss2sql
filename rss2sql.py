0cf96037d8f0f920847c66944b689c8e5d6821c3
hzy <i@huzheyang.com>
2019-01-01 11:05:12 +0800

    test on mysql and sqlite

--- rss2sql.py
+++ rss2sql.py
@@ -50,7 +50,7 @@ class SQL:
             for tbcls, tbinstance in self._tables:
                 mapper(tbcls, tbinstance)
             Session = sessionmaker(bind=self.config['sql']['engine'])
-            self.session = Session()
+            self._session = Session()
         else:
             self.logger.info('sql:field not defined')
 
@@ -58,77 +58,77 @@ class SQL:
     def tablename(self):
         return self.config['sql']['tablename']
 
+    def query(self, *args, **kwargs):
+        return self._session.query(*args, **kwargs)
+
+    def merge(self, *args, **kwargs):
+        return self._session.merge(*args, **kwargs)
+
+    def commit(self, *args, **kwargs):
+        return self._session.commit(*args, **kwargs)
+
+    def _request_sql_type(self, sql_type):
+        ret = getattr(sqlalchemy, sql_type, None)
+
+        if ret is None:
+            self.logger.critical('%s is not a valid SQL type', sql_type)
+            raise TypeError('SQL Type invalid')
+
+        return ret
+
     def _field_parse(self, field_def):
         req_type = field_def.get('type', 'TEXT')
         params = field_def.get('type_parameter', None)
         if req_type.upper() == 'REFTABLE':
             ref_tablename = '%s_REF_%s' % (self.tablename, field_def['name'])
-            pkcol = sqlalchemy.Column(
-                'id',
-                sqlalchemy.INT,
-                primary_key=True,
-                autoincrement=True,
-                nullable=False,
-            )
             req_ref_type = params[0] if isinstance(params, list) else params
             ref_params = params[1] if isinstance(params, list) else None
-            sql_type = getattr(sqlalchemy, req_ref_type, None)
-            if sql_type:
-                t_refcol = sql_type(ref_params) if ref_params else sql_type()
-                refcol = sqlalchemy.Column(
-                    field_def['name'],
-                    t_refcol,
-                    unique=True,
-                )
-            else:
-                self.logger.critical('%s is not a valid SQL type', req_type)
-                raise TypeError('SQL Type invalid')
-            ref_type = type('REF_%s' % field_def['name'], (RSS, ), {})
+            sql_type = self._request_sql_type(req_ref_type)
+            t_ref = sql_type(ref_params) if ref_params else sql_type()
+
+            pkcol = sqlalchemy.Column('id', sqlalchemy.INT, primary_key=True)
+            refcol = sqlalchemy.Column(field_def['name'], t_ref, unique=True)
+
+            ref_cls = type('REF_%s' % field_def['name'], (RSS, ), {})
             self._tables.append((
-                ref_type,
+                ref_cls,
                 sqlalchemy.Table(ref_tablename, META, pkcol, refcol),
             ))
             cargs = (
                 field_def['name'],
                 sqlalchemy.INT,
-                sqlalchemy.ForeignKey('.'.join(
-                    [ref_tablename, field_def['name']])),
+                sqlalchemy.ForeignKey('%s.id' % ref_tablename),
             )
             field_def['nullable'] = False
 
             def evalfunc(x):
                 x = eval(field_def['val'])
-                ref = self.session.query(ref_type).filter_by(
-                    **{
-                        field_def['name']: x
-                    }).first()
+                val = {field_def['name']: x}
+                ref = self.query(ref_cls).filter_by(**val).first()
                 if ref is None:
-                    self.session.merge(ref_type(**{field_def['name']: x}))
-                    self.session.commit()
-                    ref = self.session.query(ref_type).filter_by(
-                        **{
-                            field_def['name']: x
-                        }).first()
+                    self.merge(ref_cls(**{field_def['name']: x}))
+                    self.commit()
+                    ref = self.query(ref_cls).filter_by(**val).first()
                 return ref.id
         else:
-            sql_type = getattr(sqlalchemy, req_type, None)
-            if sql_type:
-                evalfunc = lambda x: eval(field_def['val'])
-                if isinstance(params, list):
-                    sql_col = sql_type(*params)
-                else:
-                    sql_col = sql_type(params) if params else sql_type()
-                cargs = (field_def['name'], sql_col)
+            sql_type = self._request_sql_type(req_type)
+
+            evalfunc = lambda x: eval(field_def['val'])
+            if isinstance(params, list):
+                sql_col = sql_type(*params)
             else:
-                self.logger.critical('%s is not a valid SQL type', req_type)
-                raise TypeError('SQL Type invalid')
+                sql_col = sql_type(params) if params else sql_type()
+            cargs = (field_def['name'], sql_col)
 
         kwargs = {
-            'nullable': field_def.get('nullable', True),
-            'primary_key': field_def.get('primary_key', False),
-            'autoincrement': field_def.get('autoincrement', False),
-            'index': field_def.get('index', False),
-            'unique': field_def.get('unique', False)
+            name: field_def.get(name, default)
+            for name, default in [
+                ('nullable', True),
+                ('primary_key', False),
+                ('autoincrement', False),
+                ('index', False),
+                ('unique', False),
+            ]
         }
 
         return (
@@ -142,9 +142,10 @@ class SQL:
             self._field_parse(f) for f in self.config['sql'].get('field', [])
         ]
         self.config['rss']['explain'] = [(i[0], i[1]) for i in fields]
-        self.config['sql']['table'] = sqlalchemy.Table(self.tablename, META,
-                                                       *[i[2] for i in fields])
-        self._tables.append((RSS, self.config['sql']['table']))
+        self._tables.append((
+            RSS,
+            sqlalchemy.Table(self.tablename, META, *[i[2] for i in fields]),
+        ))
 
     @property
     def feeds(self):
@@ -160,13 +161,13 @@ class SQL:
 
     def fetch(self):
         for feed in self.feeds['entries']:
-            self.session.merge(
+            self.merge(
                 RSS(
                     **{
                         name: func(feed)
                         for name, func in self.config['rss']['explain']
                     }))
-        self.session.commit()
+        self.commit()
 
 
 if __name__ == "__main__":
